c8b74705e60188725fec24046b81b4c72910849d
Sakshi Srivastava <sakshisrivastava413@gmail.com>
2020-06-04 19:23:12 +0530

    improvised the code alot, passing the viz name and its params to render the tool

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -2,55 +2,14 @@ export default {
   title: 'Bluegenes',
 };
 
-import interaction from '../configs/interaction-network';
-import pathway from '../configs/pathway-visualizer';
+import interactionParams from '../configs/interaction-network';
+import pathwayParams from '../configs/pathway-visualizer';
+import { renderTool } from '../util';
 
-export const InteractionNetwork = () => {
-  const elem = document.createElement('div');
-  const script = document.createElement('script');
-  const css = document.createElement('link');
-  css.rel = 'stylesheet';
-  css.href = 'http://localhost:6006/interaction-network/style.css';
-  script.src = 'http://localhost:6006/interaction-network/bundle.js';
-  elem.classList.add('bluegenesInteractionsNetwork');
-  document.head.append(css);
-  document.body.append(script);
+export const InteractionNetwork = () => (
+  renderTool('bluegenesInteractionsNetwork', interactionParams)
+);
 
-  script.onload = () => {
-    window.bluegenesInteractionsNetwork.main(
-      elem,
-      interaction.imURL,
-      interaction.dataToInitialiseToolWith,
-      interaction.toolState,
-      interaction.config,
-      interaction.navigate
-    );
-  };
-
-  return elem;
-};
-
-export const PathwayVisualizer = () => {
-  const elem = document.createElement('div');
-  const script = document.createElement('script');
-  const css = document.createElement('link');
-  css.rel = 'stylesheet';
-  css.href = 'http://localhost:6006/pathway-visualizer/style.css';
-  script.src = 'http://localhost:6006/pathway-visualizer/bundle.js';
-  elem.classList.add('bluegenesPathwayVisualizer');
-  document.head.append(css);
-  document.body.append(script);
-
-  script.onload = () => {
-    window.bluegenesPathwayVisualizer.main(
-      elem,
-      pathway.imURL,
-      pathway.dataToInitialiseToolWith,
-      pathway.toolState,
-      pathway.config,
-      pathway.navigate
-    );
-  };
-
-  return elem;
-};
+export const PathwayVisualizer = () => (
+  renderTool('bluegenesPathwayVisualizer', pathwayParams)
+);
