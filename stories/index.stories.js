efde4fda04aa685f24044f4386deaca77492e5af
Sakshi Srivastava <sakshisrivastava413@gmail.com>
2020-06-04 11:44:40 +0530

    called main function of interaction visualisation with appropriate params onload of bundleJS file

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -2,6 +2,8 @@ export default {
   title: 'Demo',
 };
 
+import { default as config } from '../config/interaction-network';
+
 export const InteractionNetwork = () => {
   const elem = document.createElement('div');
   const script = document.createElement('script');
@@ -12,5 +14,36 @@ export const InteractionNetwork = () => {
   elem.classList.add('bluegenesInteractionsNetwork');
   document.head.append(css);
   document.body.append(script);
+
+  script.onload = () => {
+
+    const imURL = {
+      root: 'https://www.humanmine.org/humanmine',
+    };
+    const dataToInitialiseToolWith = {
+      class: 'Protein',
+      format: 'id',
+      value: "128, 5468"
+    };
+    const toolState = {};
+    const navigate = function (type, data, mine) {
+      var someMine = mine ? "on ".concat(mine) : null;
+      var msg = ["Navigating to", type, someMine, "with data:"]
+        .filter(function (e) { return e })
+        .join(" ");
+      console.log(msg, data);
+    };
+
+    console.log(window.bluegenesInteractionsNetwork);
+    window.bluegenesInteractionsNetwork.main(
+      elem,
+      imURL,
+      dataToInitialiseToolWith,
+      toolState,
+      config,
+      navigate
+    );
+  };
+
   return elem;
 };
