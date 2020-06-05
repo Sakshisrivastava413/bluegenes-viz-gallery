d82fdb48fcc3d0a1d25160bb869743e03ae72fa4
Sakshi Srivastava <sakshisrivastava413@gmail.com>
2020-06-05 13:24:00 +0530

    added MSA viewer and GO term visualisation in the storybook

---
+++ configs/go-term-visualizer.js
@@ -0,0 +1,33 @@
+export default [
+    {
+        root: 'https://www.flymine.org/flymine'
+    },
+    {
+        "class": "Gene",
+        "format": "list",
+        "value": "PL_obesityMonogen_ORahilly09",
+        "value": "PL FlyTF_REDfly_TFs",
+    },
+    {},
+    {
+        "accepts": ["list"],
+        "classes": ["Gene"],
+        "columnMapping": {
+            "Gene": {
+                "id": "primaryIdentifier"
+            },
+            "Protein": {
+                "id": "primaryAccession"
+            }
+        },
+        "files": {
+            "css": "dist/style.css",
+            "js": "dist/bundle.js"
+        },
+        "toolName": {
+            "human": "BlueGenes GO-term Visualizer",
+            "cljs": "bluegenes_GO_term_visualizer"
+        }
+    }
+
+];
--- stories/index.stories.js
+++ stories/index.stories.js
@@ -7,11 +7,12 @@ import pathwayParams from '../configs/pathway-visualizer';
 import goConceptParams from '../configs/go-concept-relation';
 import diseaseExpressionArrayexpressParams from '../configs/disease-expression-arrayexpress-visualizer';
 import expressionBoxSwarmParams from '../configs/expression-box-swarm-visualizer';
-// import goTermParams from '../configs/go-term-visualizer';
+import goTermParams from '../configs/go-term-visualizer';
 import proteinAtlasTissueExpressionParams from '../configs/protein-atlas-tissue-expression-visualizer';
 // import proteinParams from '../configs/protein-visualizer';
 import tissueExpressionArrayexpressParams from '../configs/tissue-expression-arrayexpress-visualizer';
 import tissueExpressionParams from '../configs/tissue-expression-visualizer';
+import msaViewerParams from '../configs/msa-viewer';
 import { renderTool } from '../util';
 
 import imjs from 'imjs';
@@ -37,9 +38,9 @@ export const ExpressionBoxSwarmVisualizer = () => (
   renderTool('bluegenesExpressionBoxSwarmVisualizer', expressionBoxSwarmParams)
 );
 
-// export const GOTermVisualizer = () => (
-//   renderTool('bluegenes_GO_term_visualizer', goTermParams)
-// );
+export const GOTermVisualizer = () => (
+  renderTool('bluegenes_GO_term_visualizer', goTermParams)
+);
 
 export const ProteinAtlasTissueExpressionVisualizer = () => (
   renderTool('bluegenesProteinAtlasTissueExpressionVisualizer', proteinAtlasTissueExpressionParams)
@@ -55,4 +56,8 @@ export const TissueExpressionArrayexpressVisualizer = () => (
 
 export const TissueExpressionVisualizer = () => (
   renderTool('bluegenesTissueExpressionVisualizer', tissueExpressionParams)
+);
+
+export const MSAViewer = () => (
+  renderTool('bluegenesMSAViewer', msaViewerParams)
 );
