6918b668ac6a9eeffc322d027ffced6a87ec9439
Sakshi Srivastava <sakshisrivastava413@gmail.com>
2020-06-29 11:35:43 +0530

    changed storybook title to intermine. Added intermine logo

---
+++ .storybook/theme.js
@@ -0,0 +1,14 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+  base: 'light',
+  inputBg: 'white',
+  inputBorder: 'silver',
+  inputTextColor: 'black',
+  inputBorderRadius: 4,
+
+  brandImage: 'https://raw.githubusercontent.com/intermine/design-materials/f5f00be4/logos/intermine/intermine.png',
+  brandTitle: 'InterMine',
+  brandUrl: 'http://intermine.org/',
+  gridCellSize: 19
+});
