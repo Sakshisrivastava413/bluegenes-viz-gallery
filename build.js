b9516e12d6e09ce57096fd9f60f2530a55c37535
Sakshi Srivastava <sakshisrivastava413@gmail.com>
2020-07-14 22:24:32 +0530

    changed script to dynamically add the config.json, dist folder, create storybook file

--- build.js
+++ build.js
@@ -1,17 +1,56 @@
-var fs = require("fs");
-var path = require("./paths");
-var fse = require("fs-extra");
+const fs = require('fs');
+const fse = require('fs-extra');
+const package = require('./package.json');
 
-path.forEach((e) => {
-  var sourceDir = e.source;
-  var destDir = `public/${e.destination}`;
+const tools = Object.keys(package.dependencies)
+	.filter((name) => /@intermine[a-zA-Z/0-9]+/.test(name))
+	.map((name) => `node_modules/${name}`);
 
-  if (!fs.existsSync(destDir)) {
-    fs.mkdirSync(destDir, { recursive: true });
-  }
+const storybookSetup = `
+  export default {
+    title: 'Bluegenes',
+  };
+  
+  import React from 'react';
+  import ReactDOM from 'react-dom';
+  
+  window.React = React;
+  window.ReactDOM = ReactDOM;
 
-  fse.copy(sourceDir, destDir, function (err) {
-    if (err) console.error(err);
-    else console.log("success!", e.destination);
-  });
+  import imjs from 'imjs';
+  window.imjs = imjs;
+  import { renderTool } from '../util';
+  `;
+  
+fs.mkdirSync('stories', { recursive: true });
+fs.mkdirSync('configs', { recursive: true });
+
+fs.writeFileSync(`stories/index.stories.js`, storybookSetup);
+
+tools.map((tool) => {
+	const config = JSON.parse(fs.readFileSync(`${tool}/config.json`));
+	const toolName = config.toolName.cljs;
+	fs.writeFileSync(`configs/${toolName}.json`, JSON.stringify(config));
+
+	const distSource = `${tool}/dist`;
+	const distDestination = `public/${toolName}`;
+	if (!fs.existsSync(distDestination)) {
+		fs.mkdirSync(distDestination, { recursive: true });
+	}
+
+	fse.copy(distSource, distDestination, function (err) {
+		if (err) console.error(err);
+		else console.log('success!', distDestination);
+	});
+
+	const title = toolName.substr(9, toolName.length);
+
+	const text = `
+  import ${toolName}Params from '../configs/${toolName}.json';
+  export const ${title} = () => (
+    renderTool('${toolName}', ${toolName}Params)
+  );
+  `;
+
+	fs.appendFileSync(`stories/index.stories.js`, text);
 });
