b1bb8b09d65eb33144427e2dddb1a435b1158dfb
Sakshi Srivastava <sakshisrivastava413@gmail.com>
2020-07-14 19:32:52 +0530

    added navigate param in util file

--- util.js
+++ util.js
@@ -1,17 +1,26 @@
 export const renderTool = (name, params) => {
-    const baseUrl = window.location.origin;
-    const elem = document.createElement('div');
-    const script = document.createElement('script');
-    const css = document.createElement('link');
-    css.rel = 'stylesheet';
-    css.href = `${baseUrl}/${name}/style.css`;
-    script.src = `${baseUrl}/${name}/bundle.js`;
-    elem.classList.add(name);
-    document.head.append(css);
-    document.body.append(script);
+	const navigate = function (type, data, mine) {
+		var someMine = mine ? 'on '.concat(mine) : null;
+		var msg = ['Navigating to', type, someMine, 'with data:']
+			.filter(function (e) {
+				return e;
+			})
+			.join(' ');
+		console.log(msg, data);
+	};
+	const baseUrl = window.location.origin;
+	const elem = document.createElement('div');
+	const script = document.createElement('script');
+	const css = document.createElement('link');
+	css.rel = 'stylesheet';
+	css.href = `${baseUrl}/${name}/style.css`;
+	script.src = `${baseUrl}/${name}/bundle.js`;
+	elem.classList.add(name);
+	document.head.append(css);
+	document.body.append(script);
 
-    script.onload = () => {
-        window[name].main(elem, ...params);
-    };
-    return elem;
-}
+	script.onload = () => {
+		window[name].main(elem, navigate, ...params);
+	};
+	return elem;
+};
