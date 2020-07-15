6777e60b6a755d2a955b2e6b9348f53a26c997fe
Sakshi Srivastava <sakshisrivastava413@gmail.com>
2020-07-15 23:03:45 +0530

    created a file containing package name, imURL, imEntity

---
+++ tools.js
@@ -0,0 +1,115 @@
+module.exports = [
+	{
+		name: '@intermine/bluegenes-disease-expression-arrayexpress-visualizer',
+		imURL: {
+			root: 'https://www.humanmine.org/humanmine',
+		},
+		dataToInitialiseToolWith: {
+			Gene: {
+				class: 'Gene',
+				format: 'id',
+				value: 1196911,
+			},
+		},
+	},
+	{
+		name: '@intermine/bluegenes-expression-box-swarm-visualizer',
+		imURL: {
+			root: 'https://www.humanmine.org/humanmine',
+		},
+		dataToInitialiseToolWith: {
+			Gene: {
+				class: 'Gene',
+				format: 'id',
+				value: 1205472,
+			},
+		},
+	},
+	{
+		name: '@intermine/bluegenes-go-term-visualizer',
+		imURL: {
+			root: 'https://www.flymine.org/flymine',
+		},
+		dataToInitialiseToolWith: {
+			Gene: {
+				class: 'Gene',
+				format: 'ids',
+				value: [
+					1038365,
+					1011341,
+					1165074,
+					1007696,
+					1006132,
+					1043100,
+					1038456,
+					1053648,
+				],
+			},
+		},
+	},
+	{
+		name: '@intermine/bluegenes-msa-viewer',
+		imURL: {
+			root: 'https://www.humanmine.org/humanmine',
+		},
+		dataToInitialiseToolWith: {
+			Gene: {
+				class: 'Gene',
+				format: 'id',
+				value: 1196911,
+			},
+		},
+	},
+	{
+		name: '@intermine/bluegenes-protein-atlas-tissue-expression-visualizer',
+		imURL: {
+			root: 'https://www.humanmine.org/humanmine',
+		},
+		dataToInitialiseToolWith: {
+			Gene: {
+				class: 'Gene',
+				format: 'id',
+				value: 1205472,
+			},
+		},
+	},
+	{
+		name: '@intermine/bluegenes-protein-visualizer',
+		imURL: {
+			root: 'https://www.humanmine.org/humanmine',
+		},
+		dataToInitialiseToolWith: {
+			Gene: {
+				class: 'Gene',
+				format: 'id',
+				value: 1161508,
+			},
+		},
+	},
+	{
+		name: '@intermine/bluegenes-tissue-expression-arrayexpress-visualizer',
+		imURL: {
+			root: 'https://www.humanmine.org/humanmine',
+		},
+		dataToInitialiseToolWith: {
+			Gene: {
+				class: 'Gene',
+				format: 'id',
+				value: 1196911,
+			},
+		},
+	},
+	{
+		name: '@intermine/bluegenes-tissue-expression-visualizer',
+		imURL: {
+			root: 'https://www.flymine.org/flymine',
+		},
+		dataToInitialiseToolWith: {
+			Gene: {
+				class: 'Gene',
+				format: 'id',
+				value: 1007357,
+			},
+		},
+	},
+];
